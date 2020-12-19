e9e61b00c5f3158a3ecd8b52a27d2925be8e9244
AnnaPalii <annapalii@yahoo.com>
2020-12-19 08:55:34 -0500

    fixed filter

--- src/components/Userview/index.js
+++ src/components/Userview/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import API from '../../utils/API';
 
-
-
 class Userview extends React.Component{
     state = {
         searchResult: [],
@@ -20,26 +18,23 @@ class Userview extends React.Component{
             .then( res => this.setState({ filterResult: this.state.searchResult}))
     }
 
-    handleChange = event => {
+    handleInputChange = event => {
         const value = event.target.value;
         const filtered = this.state.searchResult.filter(query => query.name.first.includes(value));
         this.setState({filterResult: filtered})
     }
-    
+
     render () {
         return(
             <div>
                 <input className= "form-control mr-sm-2" onChange={this.handleInputChange} placeholder="Search Employee"></input>
-                <table className = "table table-white">
+                <table className = "table table-dark">
                     <thead>
                         <tr>
-                            
                             <th scope="col">First</th>
                             <th scope="col">Last</th>
-                            <th scope="col">Location</th>
                             <th scope="col">Email</th>
                             <th scope="col">Photo</th>
-
                         </tr>
                     </thead>
                     <tbody>
@@ -48,7 +43,6 @@ class Userview extends React.Component{
                             <tr>
                                 <th> {element.name.first} </th>
                                 <th> {element.name.last} </th>
-                                <th> {element.location.country} </th>
                                 <th> {element.email} </th>
                                 <th scope="row"><img src = {element.picture.medium}/></th>
                             </tr>
